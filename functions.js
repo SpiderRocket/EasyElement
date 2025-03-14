4fa91214cb8e7f28386040880ffc9eaeaa08cc1a
SpiderRocket <caleb.1628167@nv.ccsd.net>
2025-03-13 17:32:45 -0700

    Update and rename move.js to functions.js

---
+++ functions.js
@@ -0,0 +1,19 @@
+function EasyEl() {
+  let text = `(Header)Documentation(/Header)
+    1. Use easyEl.print("text") to easily attach text to the screen.
+  `;
+  this.text = text.replace('(Header)', '<h1>')
+  text = this.text;
+  this.text = text.replace('(/Header)', '</h1>')
+}
+EasyEl.prototype.documentation = function() {
+  this.print(this.text);
+  console.log("test");
+}
+EasyEl.prototype.print = function(printText) {
+  console.log("test");
+  const text = document.createElement("p");
+  text.innerHTML = printText;
+  document.body.appendChild(text);
+}
+let easyEl = new EasyEl();
--- move.js
+++
@@ -1,9 +0,0 @@
-EasyElement.prototype.print = function(text) {
-  const p = document.createElement("p");
-  const pNode = document.createTextNode(text);
-  p.appendChild(pNode);
-  document.body.appendChild(p);
-}
-EasyElement.prototype.documentation = function() {
-  EasyElement.print("Documentation")
-}
